4436e57923e8cec9fc9fa1f1b5ab3da46a6d536d
Olivia Caciuleanu <130154848+OliviaRocks@users.noreply.github.com>
2024-12-19 17:12:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WordCounter
+{
+    internal class TextStatistics
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text: ");
+            string inputText = Console.ReadLine();
+
+            int characterCount = inputText.Length;
+
+            string[] words = inputText.Split(new char[] { ' ', '\t', '\n', '\r' },
+        StringSplitOptions.RemoveEmptyEntries);
+            int wordCount = words.Length;
+
+            Console.WriteLine();
+            Console.WriteLine($"Number of characters: {characterCount}");
+            Console.WriteLine($"Number of words: {wordCount}");
+
+        }
+    }
+}
